51746d86953ecbd783585d10d73600c6dce24c10
Ryan Mistelbauer <c3305432@uon.edu.au>
2019-04-11 05:31:04 +0000

    Testing Decryption

--- rotationDecrypt.c
+++ rotationDecrypt.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(){
+    
+    char array[1024] = {"GIWEV XLI TPIEWIV!!!\n"};
+    char array2[1024];
+    int key = 4;
+    int index;
+    int k;
+    
+
+    for (index = 0; index < 1024; index++){
+        if (array[index] < 65 || array[index] > 90){
+            k = index;
+            array2[k] = array[index];
+            printf("%c", array2[k]);
+            } else if (array[index] - key < 65){
+               k = index;
+               array2[k] = array[index] + (26 - key);
+               printf("%c", array2[k]); 
+            } else {
+            k = index;
+            array2[k] = array[index] - key;
+            printf("%c", array2[k]);
+        }
+         
+    }
 
 return 0;
 }
